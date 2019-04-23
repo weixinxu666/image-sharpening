44fa2a182b73339f6897588abb7907f7f541c70c
weixinxu666 <40337178+weixinxu666@users.noreply.github.com>
2019-04-23 11:01:02 +0800

    Add files via upload

---
+++ img_sharpen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+import cv2 as cv
+import numpy as np
+
+
+def custom_blur_demo(image):
+    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]], np.float32)  # 锐化
+    dst = cv.filter2D(image, -1, kernel=kernel)
+    cv.imshow("demo", dst)
+    cv.imwrite("sharpened.png",dst)
+
+
+src = cv.imread("0000.png")
+cv.namedWindow("input image", cv.WINDOW_AUTOSIZE)
+cv.imshow("input image", src)
+custom_blur_demo(src)
+cv.waitKey(0)
+cv.destroyAllWindows()
